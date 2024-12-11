b49634ea0e3970f9f55562ac12df8b36bde028d2
hanseul37 <hanseul37@daum.net>
2024-12-12 05:03:37 +0900

    Inference option

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from schemas.model import Model
+from schemas.voice import Voice
 from core.train import train_model
 from core.convert import mp3_to_wav
 from core.inference import inference_voice
@@ -16,4 +17,18 @@ print(model_path, index_path)
 mp3 = 'test_tts'
 tts = mp3_to_wav(mp3)
 
-inference_voice(tts, model_path)
+voice = Voice(
+    tts = tts,
+    model_path = model_path,
+    f0_up_key = 0,
+    f0_method = "rmvpe",
+    merge_type = "median",
+    f0_autotune = False,
+    resample_sr = "0",
+    index_rate = 0.75,
+    filter_radius = 3,
+    rms_mix_rate = 0.2,
+    protect = 0.2
+)
+
+inference_voice(voice)
