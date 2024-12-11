d6ecf629084e8fd59c4b5001a9a3033aad10922c
hanseul37 <hanseul37@daum.net>
2024-12-12 04:45:19 +0900

    Training option

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ from schemas.model import Model
 from core.train import train_model
 from core.convert import mp3_to_wav
 from core.inference import inference_voice
-from core.moving import moving_directory
 
 model = Model(
     model_name = 'test1',
-    wav = 'dataset/test.wav'
+    wav = 'dataset/test.wav',
+    epoch = 10,
+    batch_size = 7
 )
 
-#model_path, index_path = train_model(model)
+model_path, index_path = train_model(model)
+print(model_path, index_path)
 
-#print(model_path, index_path)
+mp3 = 'test_tts'
+tts = mp3_to_wav(mp3)
 
-#mp3 = 'test_tts'
-#tts = mp3_to_wav(mp3)
-#model_path = 'models/RVC/test1.pth'
-
-#inference_voice(tts, model_path)
+inference_voice(tts, model_path)
--- schemas/model.py
+++ schemas/model.py
@@ -3,3 +3,5 @@ from pydantic import BaseModel
 class Model(BaseModel):
     model_name: str
     wav: str
+    epoch: int
+    batch_size: int
